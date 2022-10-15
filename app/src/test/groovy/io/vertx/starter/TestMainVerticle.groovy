d5ccf0c335fefcbd1df43a55650a918c4580c6f0
Octavio Luna <octavio.luna@gmail.com>
2022-10-14 19:25:32 -0500

    Small changes

--- app/src/test/groovy/io/vertx/starter/TestMainVerticle.groovy
+++ app/src/test/groovy/io/vertx/starter/TestMainVerticle.groovy
@@ -10,11 +10,13 @@ import org.junit.jupiter.api.extension.ExtendWith
 @ExtendWith(VertxExtension.class)
 class TestMainVerticle {
 
+  @SuppressWarnings('GrDeprecatedAPIUsage')
   @BeforeEach
   void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
     vertx.deployVerticle(new MainVerticle(), testContext.succeeding(id -> testContext.completeNow()))
   }
 
+  @SuppressWarnings('unused')
   @Test
   void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
     testContext.completeNow()
